2461ddfa6e6b14cb5b56579c6db6d9325432bd2a
byr-gdp <anthozoan77@gmail.com>
2017-03-10 16:12:12 +0800

    make binding.js iife

--- src/20170309_easy_data_binding/binding.js
+++ src/20170309_easy_data_binding/binding.js
@@ -1,41 +1,45 @@
-let domList = [];
+(function($){
+  let domList = [];
 
-let bindModal = function bindModal(obj, prop, dom) {
-  if(domList.indexOf(dom) === -1){
-    domList.push(dom);
+  let bindModal = function bindModal(obj, prop, dom) {
+    if(domList.indexOf(dom) === -1){
+      domList.push(dom);
 
-    // 针对 input、textarea 绑定 input 事件，动态更新 model
-    const nodeName = dom.nodeName.toLowerCase();
-    if(nodeName.indexOf('input') !== -1 || nodeName.indexOf('textarea') !== -1){
-      // input 可以实时更新
-      // change 可以实现懒更新
-      dom.addEventListener('input', (e) => {
-        obj[prop] = e.target.value;
-      });
+      // 针对 input、textarea 绑定 input 事件，动态更新 model
+      const nodeName = dom.nodeName.toLowerCase();
+      if(nodeName.indexOf('input') !== -1 || nodeName.indexOf('textarea') !== -1){
+        // input 可以实时更新
+        // change 可以实现懒更新
+        dom.addEventListener('input', (e) => {
+          obj[prop] = e.target.value;
+        });
+      }
     }
+
+    Object.defineProperty(obj, prop, {
+      get: () => {
+        let ret;
+
+        if('value' in domList[0]){
+          ret = domList[0].value;
+        }else if('innerText' in domList[0]){
+          ret = domList[0].innerText;
+        }
+
+        return ret;
+      },
+      set: (newVal) => {
+        domList.forEach((item) => {
+          if('value' in item){
+            item.value = newVal;
+          }else if('innerText' in item){
+            item.innerText = newVal;
+          }
+        });
+      },
+      configurable: true
+    });
   }
 
-  Object.defineProperty(obj, prop, {
-    get: () => {
-      let ret;
-
-      if('value' in domList[0]){
-        ret = domList[0].value;
-      }else if('innerText' in domList[0]){
-        ret = domList[0].innerText;
-      }
-
-      return ret;
-    },
-    set: (newVal) => {
-      domList.forEach((item) => {
-        if('value' in item){
-          item.value = newVal;
-        }else if('innerText' in item){
-          item.innerText = newVal;
-        }
-      });
-    },
-    configurable: true
-  });
-}
+  $.bindModal = bindModal;
+}(window))
